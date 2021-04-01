92aa3805d7e9139ba1af72b0ca2cf7d234f489b3
jeremi85e <jeremiesemelin@gmail.com>
2021-04-01 20:43:59 +0200

    Ajout Classement

--- projet_dev_mobile/src/components/Classement.vue
+++ projet_dev_mobile/src/components/Classement.vue
@@ -2,17 +2,44 @@
   <section class="hero">
     <div class="hero-body">
       <div class="container">
-        CECI EST LE CLASSEMENT DE {{idChampionnat}}
+        CLASSEMENT : {{classement.competition.name}}
+        <div v-for="position in classement.standings[0].table"
+            :key="position.team.id">
+          {{position.team.name}}
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Classement",
   props:{
       idChampionnat: Number,
+  },
+  data(){
+      return{
+          classement:[]
+      }
+  },
+  mounted() {
+    const url = "http://api.football-data.org/v2/competitions/" + this.idChampionnat + "/standings";
+    axios
+      .get(url, {
+            headers: {
+                'X-Auth-Token': '172572c9c3c44a3aa525d5fa18cd5457' //the token is a variable which holds the token
+            },
+        }
+      )
+      .then((response) => {
+        this.classement = response.data;
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
