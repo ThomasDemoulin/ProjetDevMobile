b04d1b5d78dd8c6a5cf8dfe44a75961e6b2881e4
jeremi85e <jeremiesemelin@gmail.com>
2021-04-01 20:57:27 +0200

    Ajout Calendrier

--- projet_dev_mobile/src/components/Calendrier.vue
+++ projet_dev_mobile/src/components/Calendrier.vue
@@ -2,17 +2,44 @@
   <section class="hero">
     <div class="hero-body">
       <div class="container">
-        CECI EST LE CALENDRIER DE {{idChampionnat}}
+        CALENDRIER : {{calendrier.competition.name}}
+        <div v-for="match in calendrier.matches"
+            :key="match.id">
+          Journée {{match.matchday}} : {{match.homeTeam.name}} - {{match.awayTeam.name}}
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: "Calendrier",
   props:{
       idChampionnat: Number,
+  },
+  data(){
+      return{
+          calendrier:{}
+      }
+  },
+  mounted() {
+    const url = "http://api.football-data.org/v2/competitions/" + this.idChampionnat + "/matches";
+    axios
+      .get(url, {
+            headers: {
+                'X-Auth-Token': '172572c9c3c44a3aa525d5fa18cd5457' //the token is a variable which holds the token
+            },
+        }
+      )
+      .then((response) => {
+        this.calendrier = response.data;
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
